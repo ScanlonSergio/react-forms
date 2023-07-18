3001d2f559791aeaf921e91aa59064bf9ad30320
Scanlon Rebelo <scanlon@edifytech.us>
2023-07-18 21:27:10 +0530

    handling simpleInput form with custom hook

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,12 +1,67 @@
+import React from "react";
+
+import useInput from "../hooks/use-input";
+
 const SimpleInput = (props) => {
+    const {
+        value: enteredName,
+        isValid: enteredNameIsValid,
+        hasError: nameInputHasError,
+        valueChangedHandler: nameChangedHandler,
+        inputBlurHandler: nameBlurHandler,
+        reset: resetNameInput
+    } = useInput(value => value.trim() !== '');
+
+    const {
+        value: enteredEmail,
+        isValid: enteredEmailIsValid,
+        hasError: emailInputHasError,
+        valueChangedHandler: emailChangedHandler,
+        inputBlurHandler: emailBlurHandler,
+        reset: resetEmailInput
+    } = useInput(value => value.includes('@'));
+
+    let formIsValid = false;
+
+    if(enteredNameIsValid && enteredEmailIsValid) {
+        formIsValid = true;
+    } 
+
+    const submitHandler = event =>{
+        event.preventDefault();
+        if(!enteredNameIsValid) {
+            return;
+        }
+        resetNameInput();
+        resetEmailInput();
+    }
+
+    const nameInputClasses = nameInputHasError ? 'form-control invalid' : 'form-control';
+    const emailInputClasses = emailInputHasError ? 'form-control invalid' : 'form-control';
+
+
     return (
-      <form>
-        <div className='form-control'>
+      <form onSubmit={submitHandler}>
+        <div className={nameInputClasses}>
           <label htmlFor='name'>Your Name</label>
-          <input type='text' id='name' />
+          <input type='text' 
+                 id='name' 
+                 onChange={nameChangedHandler} 
+                 onBlur={nameBlurHandler}
+                 value={enteredName} />
+          {nameInputHasError && <p className="error-text">Name field cannot be empty.</p>}
+        </div>
+        <div className={emailInputClasses}>
+          <label htmlFor='email'>Email</label>
+          <input type='email' 
+                 id='email' 
+                 onChange={emailChangedHandler} 
+                 onBlur={emailBlurHandler}
+                 value={enteredEmail} />
+          {emailInputHasError && <p className="error-text">Please enter a valid email.</p>}
         </div>
         <div className="form-actions">
-          <button>Submit</button>
+          <button disabled={!formIsValid}>Submit</button>
         </div>
       </form>
     );
